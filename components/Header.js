fe086f9b053567379d495573e56d2ea962977f09
bach9w <78446671+bach9w@users.noreply.github.com>
2023-10-03 16:15:55 +0300

    news

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,11 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
       <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <Image src="/logo.png" width={200} height={200} alt="logo" />
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'VM STROY';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'PERFECT BUILDING';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
